45b51743ad0bbe3aca2787966d2cbe1285fc49db
dee-oh-double-gee <beaugraham+github@gmail.com>
2017-02-24 21:40:56 -0700

    Create YouTubeSubCount.py

---
+++ YouTubeSubCount.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import requests
+import json
+import time
+import os
+
+while True:
+
+ r = requests.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=YOUTUBE_IT&key=YOUTUBE_DATA_API_V3')
+ j = r.json()
+
+ subs = j['items'][0]['statistics']['subscriberCount']
+ views = j['items'][0]['statistics']['viewCount']
+
+#while True:
+
+ time.sleep(60)
+ os.system('clear')
+ print "Beau Knows Tech... Stuff Channel Stats:"
+ print "Total number of subscribers: %s" % (subs)
+ print "Total number of views: %s" % (views)
+
